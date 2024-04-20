50ff2abbd15758d533b81762aa2ceeda784d07e9
Paulo Nadales <p.nadales.j@gmail.com>
2024-04-20 10:45:45 -0400

    Codigo comentado

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const fs = require("fs");
-
 const express = require('express');
-const { log } = require("console");
 const app = express();
 const PORT = 3000;
 
-app.listen(3000, () => console.log("Servidor escuchando Puerto: " + PORT));
+app.listen(3000, () => console.log("Servido iniciado en el puerto " + PORT));
 
+//Lee un archivo json y lo retorna como objeto
 function leer() {
     return JSON.parse(fs.readFileSync("deportes.json", "utf8"));
 }
+
+//Escribe el contenido pasado como parámetro en el archivo deportes.json
 function actualizar(datos) {
     fs.writeFileSync("deportes.json", JSON.stringify({ "deportes": datos }))
 }
 
+//Agrega los datos recibidos al archivo json
 function agregar(nombre, precio) {
     let datos = leer().deportes
     datos.push({ "nombre": nombre, "precio": precio })
     actualizar(datos)
 }
+
+//Actualiza el contenido del archivo json son los datos recibidos
 function editar(nombre, precio) {
     let datos = leer().deportes;
     let editado = datos.map(deporte => {
@@ -29,34 +33,32 @@ function editar(nombre, precio) {
     })
     actualizar(editado)
 }
+
+//Elimina los datos recibidos del archivo json
 function eliminar(nombre) {
     let datos = leer().deportes
     let noEliminados = datos.filter(deporte => deporte.nombre != nombre)
     actualizar(noEliminados)
 }
 
-// function validacionesNombre(nombre){
-//     if(nombre=='' || !/^[a-zA-Z\s]+$/.test(nombre)){
-//         return false
-//     }else{
-//         return true
-//     }
-// }
-
+// Ruta raíz con la página principal del sitio 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
+//Ruta para agregar los datos recibidos desde le front
 app.get("/agregar", (req, res) => {
+    //Se capturan los parametros de la ruta
     const { nombre, precio } = req.query
+    //Validació para que el nombre no sea vacío y solo contenga carácteres numéricos
     if (nombre == '' || !/^[a-zA-Z\s]+$/.test(nombre)) {
         return res.send("Ingrese un nombre válido")
     }
-
+    //Validación para que el precio sea un número y no sea negativo
     if (isNaN(Number(precio)) || Number(precio) < 0) {
         return res.send("Ingrese un precio válido")
     }
-
+    //Validación para no repetir deportes
     if (!leer().deportes.map(deporte => deporte.nombre).includes(nombre)) {
         agregar(nombre, precio)
         return res.sendStatus(200)
@@ -64,8 +66,11 @@ app.get("/agregar", (req, res) => {
     res.send("Ya existe el deporte")
 })
 
+//Ruta para editar con los datos recibidos desde el front
 app.get("/editar", (req, res) => {
+    //Se capturan los parametros de la ruta
     const { nombre, precio } = req.query;
+    //Validación para que el precio sea un número y no sea negativo
     if (isNaN(Number(precio)) || Number(precio) < 0) {
         return res.send("Ingrese un precio válido")
     }
@@ -75,12 +80,15 @@ app.get("/editar", (req, res) => {
 
 })
 
+//Ruta para eliminar según lo recibido desde el front
 app.get("/eliminar/:nombre", (req, res) => {
+    //Se captura el parametro de la ruta
     const nombre = req.params.nombre;
     eliminar(nombre);
     res.sendStatus(200)
 })
 
+//Ruta que retorna el contenido json del archivo deportes
 app.get("/deportes", (req, res) => {
     res.json(leer())
 }
